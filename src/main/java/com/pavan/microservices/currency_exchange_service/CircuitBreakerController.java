0a8de8ce78c2ebba097ae387e9cbe679f5173cab
SaiPavanN01 <88645382+SaiPavanN01@users.noreply.github.com>
2025-03-18 14:29:18 +0530

    Added WebClient for communication

--- src/main/java/com/pavan/microservices/currency_exchange_service/CircuitBreakerController.java
+++ src/main/java/com/pavan/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -2,6 +2,7 @@ package com.pavan.microservices.currency_exchange_service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +12,29 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class CircuitBreakerController {
 
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
 	Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	@GetMapping("/sample-api")
-//	@Retry(name="sample-api",fallbackMethod="hardcodedResponse")
-//	@CircuitBreaker(name ="sample-api",fallbackMethod="hardcodedResponse")
-//	@RateLimiter(name ="defualt")
-	@Bulkhead(name="defualt")
+	@Bulkhead(name="default")
 	public String sampleApi() {
 		logger.info("Sample api call received");
 		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8089/sum-dummy-response", String.class);
+		// Web Client
+		String response = webClientBuilder.build()
+				.get()
+				.uri("http://localhost:8089/sum-dummy-response")
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
 //		return "Sample API";
-		return entity.getBody();
+		return response;
 	}
 	
 	public String hardcodedResponse(Exception ex) {
