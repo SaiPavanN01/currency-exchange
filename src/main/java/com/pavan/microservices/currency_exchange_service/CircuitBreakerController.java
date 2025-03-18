6a9cdd19698c44eb896078f5f75cd6b99cd43725
SaiPavanN01 <88645382+SaiPavanN01@users.noreply.github.com>
2025-03-18 13:59:17 +0530

    Added Currency-Exchange Microservice

---
+++ src/main/java/com/pavan/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -0,0 +1,34 @@
+package com.pavan.microservices.currency_exchange_service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+	Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	@GetMapping("/sample-api")
+//	@Retry(name="sample-api",fallbackMethod="hardcodedResponse")
+//	@CircuitBreaker(name ="sample-api",fallbackMethod="hardcodedResponse")
+//	@RateLimiter(name ="defualt")
+	@Bulkhead(name="defualt")
+	public String sampleApi() {
+		logger.info("Sample api call received");
+		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8089/sum-dummy-response", String.class);
+//		return "Sample API";
+		return entity.getBody();
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "fallback-response";
+	}
+}
